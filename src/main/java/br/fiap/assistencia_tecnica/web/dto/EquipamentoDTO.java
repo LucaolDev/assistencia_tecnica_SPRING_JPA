732935f8c3680a2ae34eafcecdc26ad6645d67f9
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-22 22:39:43 -0300

    Equipamento

---
+++ src/main/java/br/fiap/assistencia_tecnica/web/dto/EquipamentoDTO.java
@@ -0,0 +1,20 @@
+package br.fiap.assistencia_tecnica.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EquipamentoDTO {
+    private Long id;
+    private Long idCliente;
+    private String tipo;
+    private String marca;
+    private String modelo;
+    private String numeroSerie;
+    private LocalDate dataCadastro;
+}
